dfba2760023455fbb52211d56e8fadc84317b254
anonymous anonymous <daniel.espinel@kellogg.northwestern.edu>
2022-03-14 18:25:37 +0000

    Generate attendances resource with ideas:scaffold

---
+++ app/models/attendance.rb
@@ -0,0 +1,14 @@
+class Attendance < ApplicationRecord
+  # Direct associations
+
+  # Indirect associations
+
+  # Validations
+
+  # Scopes
+
+  def to_s
+    trip.to_s
+  end
+
+end
---
+++ app/views/attendances/_attendance.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! attendance, :id, :trip_id, :attendee_id, :created_at, :updated_at
+json.url attendance_url(attendance, format: :json)
